7c3bdcb93a194192a6d3bcc9dc512c13a89bf537
Gui Lin <guileen@gmail.com>
2014-03-11 18:19:42 +0800

    refactor

--- test/models/User.js
+++ test/models/User.js
@@ -1,18 +1,49 @@
 var config = require('../config');
 var redisit = require('../../redis_model');
-var User = redisit('user', config.redis)
-  .field('username', {
-          required: true
-        , len: [4, 15]
-        , unique: true
-  })
-  .field('email', {
-          type: 'email'
-        , required: true
-        , unique: true
-  })
-  .field('avatar', {
-        default: 'foo/empty.png'
-  })
-  .field('sth', /^pattern$/)
-  .index('create_at')
+
+var User = redisit('user', {
+        entityStore: redisit.entityStore
+      , redis: require('redis').createClient();
+      , fields: {
+            id: {
+                'default': function() {
+
+                }
+            }
+          , username: {
+                required: true
+              , len: [4, 15]
+              , unique: true
+            }
+          , email: {
+                type: 'email'
+              , require: true
+              , unique: true
+            }
+          , password: {
+                required: true
+              , verify: function(value) {
+
+                }
+            }
+          , avatar: {
+                'default': 'foo/empty.png'
+            }
+          , invitor: {
+                type: 'model'
+              , model: 'user'
+              , many_to_one: 'user' // save sorted set
+            }
+        }
+      , indices: [
+            'create_at'
+        ]
+});
+
+User.on('insert', function(user) {
+
+})
+
+User.on('update', function(user) {
+
+})
